bf26c9d3157b42ad3be69a1b3ca74b45b11f960d
Tuhin Khare <tuhinkhare3@gmail.com>
2017-10-04 16:24:06 +0530

    Added Info about fromIntegral

--- Syntax in Functions/pattern_matching.hs
+++ Syntax in Functions/pattern_matching.hs
@@ -1,18 +1,23 @@
 {-
-  In the pattern matching we talk about the basic patterns that the data should conform to in order 
+  In the pattern matching we talk about the basic patterns that the data should conform to in order
   for us to write functions for specific cases
 -}
 
--- The checking is done top down 
+-- In order to kind of cast of a memeber of the "Num" typeclass to "Integral" typeclass  we use the fromIntegral function
+-- which can convert a Int to a more general number
+
+val = fromIntegral (length [1,2,3,4]) + 3.2
+
+-- The checking is done top down
 checkNum :: (Integral a) => a -> [Char]
 checkNum 9 = "The number is checked"   -- This is the base case which is checked first, specific for the number 9
 checkNum x = "The number is not checked" -- This is the general case which is checked if the base case is not satisfied
 
 {-
-  We can write many such cases one after the other, BUT ORDER OF WRITING SHOULD BE IN THE ORDER IN WHICH WE WANT IT TO BE 
+  We can write many such cases one after the other, BUT ORDER OF WRITING SHOULD BE IN THE ORDER IN WHICH WE WANT IT TO BE
   EXECUTED.
 -}
--- the order of writing matters will be shown below 
+-- the order of writing matters will be shown below
 
 factorial :: (Integral a) => a -> a
 factorial 0 = 1
@@ -20,10 +25,3 @@ factorial n = n * factorial (n-1)  -- first recursive definition
 
 -- if we want to calculate factorial(2) it goes like ==> 2 * factorial(1) ==> 2 * 1 * factorial (0) //base case exceuted here
 -- had we not written the base case above the CATCH-ALL-IN-ONE case it would be a non-terminating expression
-
-
-
-
-
-
-
