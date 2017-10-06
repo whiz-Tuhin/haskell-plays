771898d7be3506955bd72dcef6a5b93f97870df2
Tuhin Khare <tuhinkhare3@gmail.com>
2017-10-06 18:14:13 +0530

    Added let and case expressions

---
+++ Syntax in Functions/let.hs
@@ -0,0 +1,43 @@
+{-
+    here we analyse the let expressions in haskell
+    the syntax of let is somewhat like --
+
+    let <binding> in <expression> ---- which is completly opposite to the where syntax in which the binding part
+                                       takes place later on
+
+    # let is an expression per se, it means that it has its own value
+    # Highly local hence, cannot be used across guards unlike "where" construct
+-}
+
+val = 4*(let a = 9 in a+1) + 2
+
+mylist = [let square x = x*x in (square 3,square 4,square 5)]
+
+-- multiple variable bindings are also possible by using the ";" as a seperation marker
+
+tuple = (let a = 1;b =2;c = 3 in a+b+c , let foo = "Tuhin"; bar = "Khare" in foo ++ bar)
+
+-- pattern matching is also possible in the let expressions
+-- below is an example of deconstructing a triple using a let expressions
+
+triplesumtimes100  = (let (a,b,c) = (1,2,3) in (a+b+c))*100
+
+
+-- usage of let in the list comprehension in haskell
+
+calculateBMI :: [(Double,Double)] -> [Double]
+calculateBMI xs = [bmi | (w,h) <- xs, let bmi = w / h^2]  -- defining a function inside a list comprehension using the let construct
+
+
+{-
+
+      CASE EXPRESSIONS
+
+      The patter specific function declaration in haskell is a syntactic sugar for the case expressions
+
+-}
+
+describelist :: (Show a) => [a] -> String
+describelist xs = "The list is" ++ " " ++ case xs of [] -> "List is empty"
+                                                     [x] -> "singleton"
+                                                     ys -> "Long list"
