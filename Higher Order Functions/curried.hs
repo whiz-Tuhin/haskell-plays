8460f7a735c2f8267500cbe1f19ca17066e82227
Tuhin Khare <tuhinkhare3@gmail.com>
2017-10-12 11:09:43 +0530

    Added curried version for compare

--- Higher Order Functions/curried.hs
+++ Higher Order Functions/curried.hs
@@ -23,3 +23,17 @@ multiplythree x y z  = x*y*z
 
 multiplebynine =  multiplythree 2
 val = multiplebynine 3 4
+
+-- write a compare function which takes a number and compares it to 100 -- a very basic one
+
+comparewith100 :: (Num a, Ord a) => a -> Ordering
+comparewith100 x = compare 100 x -- this is not a curried version of the function its the normal one
+
+
+-- a curried version would look somewhat like
+
+comparewith100curried :: (Num a, Ord a) => a -> Ordering
+comparewith100curried = compare 100 
+
+-- in the above case the compare function has a type of (Num a, Ord a) => a -> (a -> Ordering)
+-- by passing only a single parameter we have a return of the type  (Num a, Ord a) => (a -> Ordering)
